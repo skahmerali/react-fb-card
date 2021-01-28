6dfbcd6f46b227f94fffb07ac9d400029fb2d76d
SheikhAhmer <sheikhahmerali75@gmail.com>
2021-01-28 12:32:22 +0500

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import css from './index.css';
+function MediaCard(profilePic, title, imgUrl, time, body,) {
+  return
+  <div className="root">
+    <div className ="header">
+      <div className="profilepic" src={profilePic}></div>
+      <div className="title" src={title}></div>
+      <div className="time" src={time}></div>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    </div>
+    <div className="frame">
+
+
+    <p className="body" src={body}></p>
+    <img className="img" src={imgUrl}></img>
+    </div>
+
+
+  </div>
+
+}
+// ReactDOM.render()
+
+
+
+
+
+
+ReactDOM.render(<MediaCard  profilePic ="./img/facebook.png" title ="React-Fb" time={new Date().toLocaleTimeString()}
+ body="kiya likho is me ?" imgUrl="./img/facebook-5-stars.gif"    />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
